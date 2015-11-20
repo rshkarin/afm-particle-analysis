4e1c0027c69ef195687e3ee722e289c70a1d94de
Roman Shkarin <mathii2009@gmail.com>
2015-11-20 01:35:51 +0100

    Code cleanup. Add verbosity.

--- run.py
+++ run.py
@@ -40,8 +40,10 @@ AXES_NAMES = {
 }
 
 def _get_colors(num_colors):
-    colors = [(1,1,1)] + [(random(),random(),random()) for i in xrange(255)]
-    new_cmap = matplotlib.colors.LinearSegmentedColormap.from_list('new_cmap', colors, N=num_colors)
+    colors = [(1, 1, 1)] + [(random(), random(), random()) \
+                    for i in xrange(255)]
+    new_cmap = matplotlib.colors.LinearSegmentedColormap.from_list('new_cmap', \
+                                                                    colors, N=num_colors)
 
     return new_cmap
 
@@ -67,8 +69,8 @@ def band_pass_filter(fft_data, filter_large_dia=15, filter_small_dia=5):
     for j in np.arange(1, side_len/2):
         row = j * side_len
         backrow = (side_len - j) * side_len
-        row_fact_large = np.exp(-(j*j) * scale_large);
-        row_fact_small = np.exp(-(j*j) * scale_small);
+        row_fact_large = np.exp(-(j*j) * scale_large)
+        row_fact_small = np.exp(-(j*j) * scale_small)
 
         for col in np.arange(1, side_len/2):
             backcol = side_len - col
@@ -89,17 +91,23 @@ def band_pass_filter(fft_data, filter_large_dia=15, filter_small_dia=5):
 
 
     #process meeting points (maxN/2,0) , (0,maxN/2), and (maxN/2,maxN/2)
-	rowmid = side_len * (side_len/2)
-	row_fact_large = np.exp(- (side_len/2)*(side_len/2) * scale_large)
-	row_fact_small = np.exp(- (side_len/2)*(side_len/2) * scale_small)
+    rowmid = side_len * (side_len/2)
+    row_fact_large = np.exp(- (side_len/2)*(side_len/2) * scale_large)
+    row_fact_small = np.exp(- (side_len/2)*(side_len/2) * scale_small)
 
-	fft_data[side_len/2] = fft_data[side_len/2] * (1 - row_fact_large) * row_fact_small
-	fft_data[rowmid] = fft_data[rowmid] * (1 - row_fact_large) * row_fact_small
-	fft_data[side_len/2 + rowmid] = fft_data[side_len/2 + rowmid] * (1 - row_fact_large * row_fact_large) * row_fact_small * row_fact_small
+    fft_data[side_len/2] = fft_data[side_len/2] * (1 - row_fact_large) * row_fact_small
+    fft_data[rowmid] = fft_data[rowmid] * (1 - row_fact_large) * row_fact_small
+    fft_data[side_len/2 + rowmid] = fft_data[side_len/2 + rowmid] * \
+                                    (1 - row_fact_large * row_fact_large) * \
+                                    row_fact_small * row_fact_small
 
-    filter_data[side_len/2] = filter_data[side_len/2] * (1 - row_fact_large) * row_fact_small
-    filter_data[rowmid] = filter_data[rowmid] * (1 - row_fact_large) * row_fact_small
-    filter_data[side_len/2 + rowmid] = filter_data[side_len/2 + rowmid] * (1 - row_fact_large * row_fact_large) * row_fact_small * row_fact_small
+    filter_data[side_len/2] = filter_data[side_len/2] * \
+                                    (1 - row_fact_large) * row_fact_small
+    filter_data[rowmid] = filter_data[rowmid] * \
+                                    (1 - row_fact_large) * row_fact_small
+    filter_data[side_len/2 + rowmid] = filter_data[side_len/2 + rowmid] * \
+                                       (1 - row_fact_large * row_fact_large) * \
+                                       row_fact_small * row_fact_small
 
     #loop along row 0 and side_len/2
     row_fact_large = np.exp(- (side_len/2)*(side_len/2) * scale_large)
@@ -112,12 +120,20 @@ def band_pass_filter(fft_data, filter_large_dia=15, filter_small_dia=5):
 
         fft_data[col] = fft_data[col] * (1 - col_fact_large) * col_fact_small
         fft_data[backcol] = fft_data[backcol] * (1 - col_fact_large) * col_fact_small
-        fft_data[col+rowmid] = fft_data[col+rowmid] * (1 - col_fact_large*row_fact_large) * col_fact_small*row_fact_small
-        fft_data[backcol+rowmid] = fft_data[backcol+rowmid] * (1 - col_fact_large*row_fact_large) * col_fact_small*row_fact_small
+        fft_data[col+rowmid] = fft_data[col+rowmid] * \
+                               (1 - col_fact_large*row_fact_large) * \
+                               col_fact_small*row_fact_small
+        fft_data[backcol+rowmid] = fft_data[backcol+rowmid] * \
+                                   (1 - col_fact_large*row_fact_large) * \
+                                   col_fact_small*row_fact_small
         filter_data[col] = filter_data[col] * (1 - col_fact_large) * col_fact_small
         filter_data[backcol] = filter_data[backcol] * (1 - col_fact_large) * col_fact_small
-        filter_data[col+rowmid] = filter_data[col+rowmid] * (1 - col_fact_large*row_fact_large) * col_fact_small*row_fact_small
-        filter_data[backcol+rowmid] = filter_data[backcol+rowmid] * (1 - col_fact_large*row_fact_large) * col_fact_small*row_fact_small
+        filter_data[col+rowmid] = filter_data[col+rowmid] * \
+                                  (1 - col_fact_large*row_fact_large) * \
+                                  col_fact_small*row_fact_small
+        filter_data[backcol+rowmid] = filter_data[backcol+rowmid] * \
+                                      (1 - col_fact_large*row_fact_large) * \
+                                      col_fact_small*row_fact_small
 
     #loop along column 0 and side_len/2
     col_fact_large = np.exp(- (side_len/2)*(side_len/2) * scale_large)
@@ -132,12 +148,22 @@ def band_pass_filter(fft_data, filter_large_dia=15, filter_small_dia=5):
 
         fft_data[row] = fft_data[row] * (1 - row_fact_large) * row_fact_small
         fft_data[backrow] = fft_data[backrow] * (1 - row_fact_large) * row_fact_small
-        fft_data[row+side_len/2] = fft_data[row+side_len/2] * (1 - row_fact_large*col_fact_large) * row_fact_small*col_fact_small
-        fft_data[backrow+side_len/2] = fft_data[backrow+side_len/2] * (1 - row_fact_large*col_fact_large) * row_fact_small*col_fact_small
-        filter_data[row] = filter_data[row] * (1 - row_fact_large) * row_fact_small
-        filter_data[backrow] = filter_data[backrow] * (1 - row_fact_large) * row_fact_small
-        filter_data[row+side_len/2] = filter_data[row+side_len/2] * (1 - row_fact_large*col_fact_large) * row_fact_small*col_fact_small
-        filter_data[backrow+side_len/2] = filter_data[backrow+side_len/2] * (1 - row_fact_large*col_fact_large) * row_fact_small*col_fact_small
+        fft_data[row+side_len/2] = fft_data[row+side_len/2] * \
+                                    (1 - row_fact_large*col_fact_large) * \
+                                                row_fact_small*col_fact_small
+        fft_data[backrow+side_len/2] = fft_data[backrow+side_len/2] * \
+                                       (1 - row_fact_large*col_fact_large) * \
+                                       row_fact_small*col_fact_small
+        filter_data[row] = filter_data[row] * \
+                                    (1 - row_fact_large) * row_fact_small
+        filter_data[backrow] = filter_data[backrow] * \
+                                        (1 - row_fact_large) * row_fact_small
+        filter_data[row+side_len/2] = filter_data[row+side_len/2] * \
+                                      (1 - row_fact_large*col_fact_large) * \
+                                      row_fact_small*col_fact_small
+        filter_data[backrow+side_len/2] = filter_data[backrow+side_len/2] * \
+                                          (1 - row_fact_large*col_fact_large) * \
+                                          row_fact_small*col_fact_small
 
     fft_data = np.reshape(fft_data, fft_data_shape)
     filter_data = np.reshape(filter_data, fft_data_shape)
@@ -167,28 +193,36 @@ def process_stats(particles_stats, pixel_scale_factor=0.512):
         stats_columns.remove('label')
 
     def scale_values(item):
-        if isinstance(item,tuple):
+        if isinstance(item, tuple):
             return tuple(x / pixel_scale_factor for x in item)
         else:
             return item / pixel_scale_factor
 
     particles_stats_scaled = particles_stats.copy()
-    particles_stats_scaled[stats_columns] = particles_stats_scaled[stats_columns].applymap(scale_values)
+    particles_stats_scaled[stats_columns] = \
+                particles_stats_scaled[stats_columns].applymap(scale_values)
 
     return particles_stats_scaled, particles_stats_scaled.columns.values
 
-def segment_data(data, min_distance=5, footprint=disk(10), max_filt_footprint=disk(5), indices=False):
+def segment_data(data, min_distance=5, footprint=disk(10), \
+                 max_filt_footprint=disk(5), indices=False):
     th_val = filters.threshold_otsu(data)
     thresholded_particles = data > th_val
     distance = ndi.distance_transform_edt(thresholded_particles)
-    distance = ndi.maximum_filter(distance, footprint=max_filt_footprint, mode='nearest')
-    local_maxi = peak_local_max(distance, min_distance=min_distance, indices=indices, footprint=footprint, labels=thresholded_particles)
+    distance = ndi.maximum_filter(distance, \
+                                  footprint=max_filt_footprint, \
+                                  mode='nearest')
+    local_maxi = peak_local_max(distance, min_distance=min_distance, \
+                                indices=indices, footprint=footprint, \
+                                labels=thresholded_particles)
     labeled_data, num_features = ndi.measurements.label(local_maxi)
-    segmented_data = watershed(-distance, labeled_data, mask=thresholded_particles)
+    segmented_data = watershed(-distance, labeled_data, \
+                                mask=thresholded_particles)
 
     return segmented_data, local_maxi
 
-def preprocess_data(data, small_particle=5, large_particle=15, min_exposure=5, max_exposure=95):
+def preprocess_data(data, small_particle=5, large_particle=15, \
+                    min_exposure=5, max_exposure=95):
     height, width = data.shape
     pad_height, pad_width = next_length_pow2(height + 1), next_length_pow2(width + 1)
 
@@ -202,17 +236,22 @@ def preprocess_data(data, small_particle=5, large_particle=15, min_exposure=5, m
 
     fft_data = np.fft.fft2(padded_data)
 
-    filtered_fft_data, filter_data = band_pass_filter(fft_data, large_particle, small_particle)
+    filtered_fft_data, filter_data = band_pass_filter(fft_data, \
+                                                      large_particle, \
+                                                      small_particle)
 
     ifft_data = np.fft.ifft2(filtered_fft_data)
     filtered_data = ifft_data.real[crop_bbox].astype(np.float32)
 
     p1, p2 = np.percentile(filtered_data, (min_exposure, max_exposure))
-    filtered_rescaled_data = exposure.rescale_intensity(filtered_data, in_range=(p1, p2))
+    filtered_rescaled_data = exposure.rescale_intensity(filtered_data, \
+                                                        in_range=(p1, p2))
 
     return filtered_rescaled_data
 
-def create_histogram_figure(stats, output_path, column='avg_axis', range=[], color='r', figsize=(8,6), bins=20, language='en'):
+def create_histogram_figure(stats, output_path, column='avg_axis', range=[], \
+                            color='r', figsize=(8,6), bins=20, language='en', \
+                            verbose=False):
     base_filename='histogram'
     filename_suffix = '.png'
 
@@ -223,15 +262,21 @@ def create_histogram_figure(stats, output_path, column='avg_axis', range=[], col
                                        (stats[column] <= np.max(range))]
     fig, ax = plt.subplots()
     fig.set_size_inches(figsize)
-    counts, bins, patches = ax.hist(filtered_data.values, bins=bins, color=color)
+    counts, bins, patches = ax.hist(filtered_data.values, \
+                                    bins=bins, \
+                                    color=color)
     ax.set_xticks(bins)
     ax.xaxis.set_major_formatter(FormatStrFormatter('%0.f'))
     plt.xlabel(AXES_NAMES[column][language])
     plt.ylabel(AXES_NAMES['frequency'][language])
-    plt.savefig(os.path.join(output_path, base_filename + '_' + column + filename_suffix), bbox_inches='tight')
-    #plt.show()
+    plt.savefig(os.path.join(output_path, base_filename + '_' + \
+                             column + filename_suffix), bbox_inches='tight')
+    if verbose:
+        plt.show()
 
-def create_overlay_figure(data, data_mask, label_stats, filename, output_path, base_filename='label_overlay', filename_suffix='.png', figsize=(8,8)):
+def create_overlay_figure(data, data_mask, label_stats, filename, \
+                          output_path, base_filename='label_overlay', \
+                          filename_suffix='.png', figsize=(8,8), verbose=False):
     if not len(label_stats.index):
         log.critical('No data stats collected.')
         sys.exit(1)
@@ -242,11 +287,16 @@ def create_overlay_figure(data, data_mask, label_stats, filename, output_path, b
     ax.autoscale(False)
     ax.set_adjustable('box-forced')
     ax.set_axis_off()
-    ax.imshow(data_mask, alpha=0.3, cmap=_get_colors(len(label_stats.index)), interpolation='bicubic')
-    plt.savefig(os.path.join(output_path, '_'.join([filename, base_filename]) + filename_suffix), bbox_inches='tight')
-    #plt.show()
+    ax.imshow(data_mask, alpha=0.3, cmap=_get_colors(len(label_stats.index)), \
+              interpolation='bicubic')
+    plt.savefig(os.path.join(output_path, '_'.join([filename, base_filename]) + \
+                filename_suffix), bbox_inches='tight')
+    if verbose:
+        plt.show()
 
-def create_axis_figure(data, label_stats, filename, output_path, base_filename='axis', file_ext='.png', figsize=(8,8)):
+def create_axis_figure(data, label_stats, filename, output_path, \
+                       base_filename='axis', file_ext='.png', figsize=(8,8), \
+                       verbose=False):
     fig = plt.figure(figsize=figsize)
     ax = fig.add_axes([.0, .0, 1.0, 1.0])
     ax.imshow(data, cmap=cm.gray)
@@ -266,13 +316,20 @@ def create_axis_figure(data, label_stats, filename, output_path, base_filename='
         ax.plot((x0, x2), (y0, y2), '-r', linewidth=1.5)
         ax.plot(x0, y0, '.g', markersize=5)
 
-        approx_particle = Circle((x0, y0), radius=row.avg_axis*0.5, edgecolor='b', linewidth=1, fill=False)
+        approx_particle = Circle((x0, y0), \
+                                 radius=row.avg_axis*0.5, \
+                                 edgecolor='b', \
+                                 linewidth=1, \
+                                 fill=False)
         ax.add_patch(approx_particle)
 
-    plt.savefig(os.path.join(output_path, '_'.join([filename, base_filename]) + file_ext), bbox_inches='tight')
-    #plt.show()
+    plt.savefig(os.path.join(output_path, \
+                             '_'.join([filename, base_filename]) + file_ext), \
+                             bbox_inches='tight')
+    if verbose:
+        plt.show()
 
-def process_sample(sample):
+def process_sample(sample, verbose=False):
     log.info("### %s is being processed... ###" % sample['name'])
     afm_image = nanoscope.read(os.path.join(sample['input_path'], sample['filename']))
     data = afm_image.height.data
@@ -299,13 +356,16 @@ def process_sample(sample):
     label_stats = particles_stats(segmented_data, properties)
 
     log.info("----- Processing of particle statistics...")
-    processed_stats, columns = process_stats(label_stats, pixel_scale_factor=sample['pixel_scale'])
+    processed_stats, columns = process_stats(label_stats, \
+                                        pixel_scale_factor=sample['pixel_scale'])
 
     log.info("----- Overlay image creation...")
-    create_overlay_figure(data, segmented_data, label_stats, sample['name'], sample['output_path'])
+    create_overlay_figure(data, segmented_data, label_stats, \
+                          sample['name'], sample['output_path'], verbose=verbose)
 
     log.info("----- Particle's axes image creation...")
-    create_axis_figure(data, label_stats, sample['name'], sample['output_path'])
+    create_axis_figure(data, label_stats, sample['name'], \
+                       sample['output_path'], verbose=verbose)
 
     log.info("----- Histogram plotting...")
     create_histogram_figure(processed_stats, \
@@ -314,11 +374,13 @@ def process_sample(sample):
                             range=sample['histogram']['range'], \
                             bins=sample['histogram']['bins'], \
                             language=sample['histogram']['lang'], \
-                            figsize=tuple(sample['histogram']['figsize']))
+                            figsize=tuple(sample['histogram']['figsize']),
+                            verbose=verbose)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("param", help="path to file of samples' parameters (e.g. param.yaml)")
+    parser.add_argument("-v", "--verbose", action="store_true", help="show images and charts")
     args = parser.parse_args()
 
     if args.param:
@@ -329,7 +391,7 @@ def main():
                     samples = config['samples']
 
                     for sample in samples:
-                        process_sample(sample)
+                        process_sample(sample, verbose=args.verbose)
 
                 except KeyError:
                     print "No samples in configuration file."
@@ -337,25 +399,6 @@ def main():
         except IOError:
             print "Can't open %s" % args.param
             sys.exit(1)
-
-# def main():
-#     root_folder_path = "/Users/rshkarin/Documents/AllaData"
-#     afm_image = nanoscope.read(os.path.join(root_folder_path, 'data.000'))
-#     data = afm_image.height.data
-#     # data_path = os.path.join(root_folder_path, "afm_data_16bit_512x512.raw")
-#     # data = np.memmap(data_path, dtype=np.int16, shape=(512,512), mode='r')
-#     properties=['label','area','centroid','equivalent_diameter','major_axis_length','minor_axis_length','orientation','bbox']
-#
-#     processed_data = preprocess_data(data)
-#     segmented_data, local_maxi = segment_data(processed_data)
-#     label_stats = particles_stats(segmented_data, properties)
-#     processed_stats, columns = process_stats(label_stats)
-#
-#     output_path = root_folder_path
-#
-#     create_overlay_figure(data, segmented_data, label_stats, "sample0001", output_path)
-#     create_axis_figure(data, label_stats, "sample0001", output_path)
-#     create_histogram_figure(processed_stats, output_path, range=range(10,400), bins=20, language='en')
 
 if __name__ == "__main__":
     sys.exit(main())
